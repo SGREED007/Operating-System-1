02eefaded1ea13b59c5b559947af89f0ae099252
SHUBHAM007 <ss22112003@gmail.com>
2024-11-26 13:27:52 +0530

    Create code.c

---
+++ Readers-Writers Problem/code.c
@@ -0,0 +1,66 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+sem_t rw_mutex, mutex;
+int read_count = 0;
+
+void *reader(void *arg) {
+    int reader_id = *((int *)arg);
+
+    sem_wait(&mutex);
+    read_count++;
+    if (read_count == 1) {
+        sem_wait(&rw_mutex);
+    }
+    sem_post(&mutex);
+
+    printf("Reader %d is reading\n", reader_id);
+
+    sem_wait(&mutex);
+    read_count--;
+    if (read_count == 0) {
+        sem_post(&rw_mutex);
+    }
+    sem_post(&mutex);
+
+    return NULL;
+}
+
+void *writer(void *arg) {
+    int writer_id = *((int *)arg);
+
+    sem_wait(&rw_mutex);
+    printf("Writer %d is writing\n", writer_id);
+    sem_post(&rw_mutex);
+
+    return NULL;
+}
+
+int main() {
+    pthread_t readers[5], writers[2];
+    int reader_ids[5] = {1, 2, 3, 4, 5};
+    int writer_ids[2] = {1, 2};
+
+    sem_init(&rw_mutex, 0, 1);
+    sem_init(&mutex, 0, 1);
+
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&readers[i], NULL, reader, &reader_ids[i]);
+    }
+    for (int i = 0; i < 2; i++) {
+        pthread_create(&writers[i], NULL, writer, &writer_ids[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        pthread_join(readers[i], NULL);
+    }
+    for (int i = 0; i < 2; i++) {
+        pthread_join(writers[i], NULL);
+    }
+
+    sem_destroy(&rw_mutex);
+    sem_destroy(&mutex);
+
+    return 0;
+}
